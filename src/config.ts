b2e165cf38198b59dc926657333233304fcf1998
wbeatty <48626588+wbeatty@users.noreply.github.com>
2025-11-19 02:17:32 -0500

    fixed

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
-export const clientId = "384ab0bd9b9c41269201bd84a816008b"; // User will need to replace this
+export const clientId = "YOUR_CLIENT_ID_HERE"; // User will need to replace this
 export const redirectUri = "http://127.0.0.1:5173/callback";
 export const authEndpoint = "https://accounts.spotify.com/authorize";
 export const scopes = [
